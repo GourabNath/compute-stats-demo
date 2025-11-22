7c8e82b9c3fbebe58595e92c839ca37c42ac9ee0
GourabNath <gourabnath88@gmail.com>
2025-11-22 20:25:04 +0000

    feat: add compute_stats v0.1.0

---
+++ src/compute_stats.py
@@ -0,0 +1,16 @@
+
+__version__ = "0.1.0"
+
+def compute_stats(numbers):
+    """Return (n, mean, std). Accepts list of floats/ints."""
+    n = len(numbers)
+    if n == 0:
+        return 0, None, None
+    mean = sum(numbers)/n
+    var = sum((x-mean)**2 for x in numbers)/n
+    std = var**0.5
+    return n, mean, std
+
+if __name__ == '__main__':
+    sample = [1,2,3,4,5]
+    print('v', __version__, '->', compute_stats(sample))
